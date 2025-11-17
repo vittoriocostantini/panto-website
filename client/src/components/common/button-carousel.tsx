501d42de1807f6a856f1f91e9426aca09baa4b92
costantini <penav1058@gmail.com>
2025-11-17 09:57:14 -0400

    feat: product carousel cards feat: data assets products feat: select bar material ui categories

---
+++ client/src/components/common/button-carousel.tsx
@@ -0,0 +1,25 @@
+import { ArrowRightAlt } from "@mui/icons-material";
+
+interface ButtonCarouselProps {
+  onClick: () => void;
+  direction: "left" | "right";
+  ariaLabel: string;
+}
+
+const ButtonCarousel = ({ onClick, direction, ariaLabel }: ButtonCarouselProps) => {
+  const positionClass = direction === "left" ? "left-10" : "right-10";
+  const iconRotation = direction === "left" ? "rotate(180deg)" : "none";
+
+  return (
+    <button
+      onClick={onClick}
+      className={`absolute ${positionClass} top-1/2 cursor-pointer  -translate-y-1/2 z-10 w-12 h-12 rounded-full bg-white  flex items-center justify-center hover:bg-gray-50 transition-colors`}
+      aria-label={ariaLabel}
+    >
+      <ArrowRightAlt sx={{ fontSize: 30, transform: iconRotation }} />
+    </button>
+  );
+};
+
+export default ButtonCarousel;
+
