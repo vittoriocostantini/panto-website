4355620f2dbbd586fca197069294f4086e2be3b2
costantini <penav1058@gmail.com>
2025-11-23 12:34:33 -0400

    fix routes vercel

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,7 @@ const allowedOrigins = [
   "http://localhost:5000",
   "http://localhost:3001",
   "http://localhost:5173",
-  "https://panto-website-xg2s.vercel.app/",
+  "https://panto-website-xg2s.vercel.app",
   "http://localhost:3000",
 ];
 
