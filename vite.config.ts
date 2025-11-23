730c9d56942b2866e418f8d73b107b06511a733f
costantini <penav1058@gmail.com>
2025-11-23 12:09:29 -0400

    fix root extension

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: './client',
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: '../dist'
+  }
 })
